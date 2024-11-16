dd9ddb359f02515b3d1b5dd7ac3361e55f3344e9
Mike Winger <mike.winger87@gmail.com>
2024-11-15 17:51:57 -0700

    added moving balls (no collision yet)

--- src/main.cpp
+++ src/main.cpp
@@ -29,6 +29,22 @@ For a C++ project simply rename the file to .cpp and re-run the build script
 #include "resource_dir.h"	// utility header for SearchAndSetResourceDir
 #include "rlgl.h"
 
+struct Ball {
+	float x;
+	float y;
+	int radius;
+	int dir;
+};
+
+const int ballCount = 5;
+int ballSpeed = 50;
+
+Ball balls[ballCount];
+
+void DrawBalls();
+void InitBalls();
+void MoveBalls();
+
 int main ()
 {
 	// Tell the window to use vsync and work on high DPI displays
@@ -39,6 +55,8 @@ int main ()
 
 	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
 	SearchAndSetResourceDir("resources");
+
+	InitBalls();
 	
 	// game loop
 	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
@@ -55,6 +73,10 @@ int main ()
 		rlRotatef(90, 1, 0, 0);
 		DrawGrid(1000, 25);
 		rlPopMatrix();
+
+		MoveBalls();
+
+		DrawBalls();
 		
 		// end the frame and get ready for the next one  (display frame, poll input, etc...)
 		EndDrawing();
@@ -63,4 +85,24 @@ int main ()
 	// destroy the window and cleanup the OpenGL context
 	CloseWindow();
 	return 0;
+}
+
+void InitBalls() {
+	for (int i = 0; i < ballCount; i++) {
+		balls[i] = { (i + 1) * 50.f, (i + 1) * 50.f, 10, i % 4 };
+	}
+}
+
+void MoveBalls() {
+	for (int i = 0; i < ballCount; i++) {
+		float time = GetFrameTime();
+		balls[i].x += ballSpeed * time * (balls[i].dir == 0 || balls[i].dir == 2 ? 1 : -1);
+		balls[i].y += ballSpeed * time * (balls[i].dir == 1 || balls[i].dir == 3 ? 1 : -1);
+	}
+}
+
+void DrawBalls() {
+	for (int i = 0; i < ballCount; i++) {
+		DrawCircle(balls[i].x, balls[i].y, balls[i].radius, RED);
+	}
 }
